e16ebfff3ca9bede7477722684d8048ffcf72ea7
newsarawut <sarawutch572@gmail.com>
2024-05-14 01:34:49 +0700

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,20 @@
+import Collections from "@/components/Collections";
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Image
+        src="/banner.jpeg"
+        alt="banner"
+        width={2000}
+        height={1000}
+        className="w-screen"
+        priority={true}
+      />
+      <Collections />
+      <ProductList />
+    </>
+  );
+}
---
+++ components/Gallery.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import Image from "next/image";
+import { useState } from "react";
+
+const Gallery = ({ productMedia }: { productMedia: string[] }) => {
+    const [mainImage, setMainImage] = useState(productMedia[0])
+
+  return (
+    <div className="flex flex-col gap-3 max-w-[500px]">
+      <Image
+        src={mainImage}
+        width={550}
+        height={500}
+        alt="product"
+        className="w-full h-[520px] rounded-lg shadow-xl object-cover"
+      />
+      <div className="flex gap-2 overflow-auto tailwind-scrollbar-hide">
+        {productMedia.map((image, index) => (
+          <Image
+            key={index}
+            src={image}
+            alt="product"
+            width={200}
+            height={200}
+            className={`w-20 h-20 rounded-lg object-cover cursor-pointer ${mainImage === image ? "border-2 border-black " : "" }`}
+            onClick={() => setMainImage(image)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
